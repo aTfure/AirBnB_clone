e54aff8bcaee38ad3675d36f7ac16937d541c712
Tom-254 <211mseeratiego@gmail.com>
2022-10-28 22:58:26 +0000

    redirections

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,51 @@
 #!/usr/bin/python3
+"""Module that defines all common attributes/methods for other classes """
+from uuid import uuid4
+from datetime import datetime
 
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the BaseModel instance attributes
+            Args:
+                *args: tuple of positional arguments
+                **kwargs: dict of keyword arguments
+        """
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "__class__":
+                    continue
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """returns [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute
+            updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing
+            all keys/values of __dict__ of the instance
+        """
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = type(self).__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,2 +1,58 @@
 #!/usr/bin/python3
+"""Module serializes instances to a JSON file and
+deserializes JSON file to instances
+"""
+import json
 
+from models.base_model import BaseModel
+
+class FileStorage:
+    """
+    serializes instances to a JSON
+    file and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with
+        key <obj class name>.id
+        """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+            serializes __objects to the JSON file
+            (path: __file_path)
+        """
+        tmp_dict = {}
+        for key, value in self.__objects.items():
+            tmp_dict[key] = value.to_dict()
+
+        with open(self.__file_path, "w", encoding="utf-8") as write_file:
+            json.dump(tmp_dict, write_file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists
+        """
+        defined_classes = {
+            "BaseModel" : BaseModel
+        }
+        try:
+            tmp_dict = {}
+            with open(self.__file_path, "r") as read_file:
+                for key, value in json.load(read_file).items():
+                    tmp_dict[key] = defined_classes[value["__class__"]](**value)
+
+            self.__objects = tmp_dict
+
+        except Exception as ex:
+            pass
