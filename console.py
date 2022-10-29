29806940f86d86605fbbe834f5c640d0476daac4
Tom-254 <211mseeratiego@gmail.com>
2022-10-29 19:42:50 +0000

    redirections

--- console.py
+++ console.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+"""contains the entry point of the command interpreter
+"""
+import cmd
 
+class HBNBCommand(cmd.Cmd):
+    """entry point of the command interpreter
+    """
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        """exit the program
+        """
+        return True
+
+    def do_quit(self, line):
+        """exit the program
+        """
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
