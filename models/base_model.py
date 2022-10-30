eb0ea7c46e2a5175f86eaab1ed00880906332c80
Tom-254 <211mseeratiego@gmail.com>
2022-10-30 23:37:35 +0000

    redirections

--- models/base_model.py
+++ models/base_model.py
@@ -36,6 +36,12 @@ class BaseModel:
         return "[{}] ({}) {}".format(type(self).__name__,
                                      self.id, self.__dict__)
 
+    def __repr__(self):
+        """returns [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__)
+
     def save(self):
         """updates the public instance attribute
             updated_at with the current datetime
